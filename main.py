4627164f43aa83a094ee2440f6af46a2f1b4220d
Eyad El Hajj <eelh0002@student.monash.edu>
2025-09-29 12:21:52 +1000

    Fixed up Load

--- main.py
+++ main.py
@@ -13,6 +13,23 @@ alt_rows["location_name"] = "Côte d’Ivoire"   # curly apostrophe
 alt_rows2 = ivory_rows.copy()
 alt_rows2["location_name"] = "Ivory Coast"   # English alias
 
+
+
+# ----------------------------
+# Laos aliases
+# ----------------------------
+laos_rows = df[df["location_name"] == "Lao People's Democratic Republic"]
+
+alt_rows_laos1 = laos_rows.copy()
+alt_rows_laos1["location_name"] = "Laos"
+
+alt_rows_laos2 = laos_rows.copy()
+alt_rows_laos2["location_name"] = "Lao PDR"
+
+alt_rows_laos3 = laos_rows.copy()
+alt_rows_laos3["location_name"] = "Lao Peoples Democratic Republic"  # missing apostrophe
+
+
 # Append them to the dataframe
 df_updated = pd.concat([df, alt_rows, alt_rows2], ignore_index=True)
 
